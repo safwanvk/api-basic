bcc44c3921fe1c62af061988c351fdee5acf5138
safwanvk <safwanvalakundil@gmail.com>
2020-09-07 17:15:30 +0530

    :zap: Set hello-world

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World"}
+
+
+api.add_resource(HelloWorld, '/hello-world')
 if __name__ == "__main__":
     app.run(debug=True)
