b9224b3ff989d8d80078807a9f7f1fd63f218af9
safwanvk <safwanvalakundil@gmail.com>
2020-09-07 17:37:48 +0530

    :zap: Setup dict

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {'ajas': {'age': 21, 'gender': 'male'},
+         'ali': {'age': 22, 'gender': 'male'}}
+
 
 class HelloWorld(Resource):
-    def get(self, name, age):
-        return {'name': name, 'age': age}
+    def get(self, name):
+        return names[name]
 
 
-api.add_resource(HelloWorld, '/hello-world/<string:name>/<int:age>')
+api.add_resource(HelloWorld, '/hello-world/<string:name>')
 if __name__ == "__main__":
     app.run(debug=True)
