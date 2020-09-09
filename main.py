067a23ff69d8cf201c5fcde993fe35032d6037d4
safwanvk <safwanvalakundil@gmail.com>
2020-09-09 11:44:53 +0530

    :zap: Setup app

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {'ajas': {'age': 21, 'gender': 'male'},
-         'ali': {'age': 22, 'gender': 'male'}}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument('name', type=str, help='Name of the video', required=True)
+video_put_args.add_argument('views', type=int, help='Views of the video', required=True)
+video_put_args.add_argument('likes', type=int, help='Likes of the video', required=True)
 
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+videos = {}
 
 
-api.add_resource(HelloWorld, '/hello-world/<string:name>')
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+
+api.add_resource(Video, '/video/<int:video_id>')
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import requests
 
 BASE = 'http://127.0.0.1:5000/'
-response = requests.get(BASE + "hello-world/ajas")
+response = requests.put(BASE + "video/1", {'name': 'seban', 'views': 10, 'likes': 10})
+response = requests.get(BASE + "video/1")
 print(response.json())
