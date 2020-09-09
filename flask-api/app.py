e45a50e64e0404e53fb1cb84dfeb9410da35b29a
safwanvk <safwanvalakundil@gmail.com>
2020-09-09 19:29:20 +0530

    :zap: Set get_product

--- flask-api/app.py
+++ flask-api/app.py
@@ -65,5 +65,11 @@ def get_products():
     return jsonify(result)
 
 
+@app.route('/product/<id>', methods=['GET'])
+def get_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
