5f3c08c9ac2d659e60c502abc42e80988a952d0d
safwanvk <safwanvalakundil@gmail.com>
2020-09-09 19:37:30 +0530

    :zap: Set update_product

--- flask-api/app.py
+++ flask-api/app.py
@@ -71,5 +71,23 @@ def get_product(id):
     return product_schema.jsonify(product)
 
 
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.qty = qty
+
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
